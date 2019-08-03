6ea51459925cb1ac7bad01fdcc75766fe5f34a9a
Aviv Haruzi <avivharuzi@gmail.com>
2019-08-03 12:09:07 +0300

    Update dijkstras algorithm

--- data-structures/dijkstras-algorithm/README.md
+++ data-structures/dijkstras-algorithm/README.md
@@ -1,6 +1,6 @@
 # Dijkstra's Algorithm
 
-* One of the most famous and widely used algorithms around!
+* One of the most famous and widely used algorithms around
 * Finds the shortest path between two vertices on a graph
 * "What's the fastest way to get from point A to point B?"
 
@@ -8,7 +8,7 @@
 
 * Edsger Dijkstra was a Dutch programmer, physicist, essayist, and all around smarty-pants
 * He helped to advance the field of computer science from an "art" to an academic discipline
-* Many of his discoveries and algorithms are still commonly used to this day.
+* Many of his discoveries and algorithms are still commonly used to this day
 
 ## Why Is It Useful?
 
@@ -16,11 +16,10 @@
 * Network Routing - finds open shortest path for data
 * Biology - used to model the spread of viruses among humans
 * Airline tickets - finding cheapest route to your destination
-* Many other uses!
 
 # The Approach
 
-1. Every time we look to visit a new node, we pick the node with the smallest known distance to visit first.
+1. Every time we look to visit a new node, we pick the node with the smallest known distance to visit first
 2. Once we’ve moved to the node we’re going to visit, we look at each of its neighbors
-3. For each neighboring node, we calculate the distance by summing the total edges that lead to the node we’re checking from the starting node.
-4. If the new total distance to a node is less than the previous total, we store the new shorter distance for that node.
+3. For each neighboring node, we calculate the distance by summing the total edges that lead to the node we’re checking from the starting node
+4. If the new total distance to a node is less than the previous total, we store the new shorter distance for that node
--- data-structures/dijkstras-algorithm/dijkstras-algorithm.js
+++ data-structures/dijkstras-algorithm/dijkstras-algorithm.js
@@ -1,46 +1,113 @@
+/**
+ * Class representing a priority queue.
+ *
+ * @class
+ */
 class PriorityQueue {
+  /**
+   * Create a priority queue.
+   *
+   * @constructor
+   */
   constructor() {
+    /** @property {Array} */
     this.values = [];
   }
+
+  /**
+   * Enqueue value into values array.
+   *
+   * @param {*} val
+   * @param {number} priority
+   */
   enqueue(val, priority) {
     this.values.push({
       val,
       priority
     });
+
     this.sort();
   };
+
+  /**
+   * Dequeue value from values array.
+   *
+   * @return {*}
+   */
   dequeue() {
     return this.values.shift();
   };
+
+  /**
+   * Sort values array.
+   */
   sort() {
     this.values.sort((a, b) => a.priority - b.priority);
   };
 }
 
+/**
+ * Class representing a weighted graph.
+ *
+ * @class
+ */
 class WeightedGraph {
+  /**
+   * Create a weighted graph.
+   *
+   * @constructor
+   */
   constructor() {
+    /** @property {Object} */
     this.adjacencyList = {};
   }
+
+  /**
+   * Add vertex to adjacencyList.
+   *
+   * @param {*} vertex - The vertex value.
+   */
   addVertex(vertex) {
-    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
   }
+
+  /**
+   * Add connection between two vertices with weight number.
+   *
+   * @param {*} vertex1 - The vertex1 value.
+   * @param {*} vertex2 - The vertex2 value.
+   * @param {number} weight - The weight value.
+   */
   addEdge(vertex1, vertex2, weight) {
     this.adjacencyList[vertex1].push({
       node: vertex2,
       weight
     });
+
     this.adjacencyList[vertex2].push({
       node: vertex1,
       weight
     });
   }
-  Dijkstra(start, finish) {
-    const nodes = new PriorityQueue();
-    const distances = {};
-    const previous = {};
-    let path = [] //to return at end
-    let smallest;
-    //build up initial state
+
+  /**
+   * Finds the shortest path using dijkstra algorithm.
+   *
+   * @param {*} start - The start value.
+   * @param {*} finish - The finish value.
+   * @return {Array}
+   */
+  dijkstra(start, finish) {
+    const nodes = new PriorityQueue(),
+      distances = {},
+      previous = {};
+
+    let path = [],
+      smallest;
+
+    // Build up initial state.
     for (let vertex in this.adjacencyList) {
       if (vertex === start) {
         distances[vertex] = 0;
@@ -49,60 +116,48 @@ class WeightedGraph {
         distances[vertex] = Infinity;
         nodes.enqueue(vertex, Infinity);
       }
+
       previous[vertex] = null;
     }
-    // as long as there is something to visit
+
+    // As long as there is something to visit.
     while (nodes.values.length) {
       smallest = nodes.dequeue().val;
+
       if (smallest === finish) {
-        //WE ARE DONE
-        //BUILD UP PATH TO RETURN AT END
+        // We are done.
+        // Build up path to return at end.
         while (previous[smallest]) {
           path.push(smallest);
           smallest = previous[smallest];
         }
+
         break;
       }
+
       if (smallest || distances[smallest] !== Infinity) {
         for (let neighbor in this.adjacencyList[smallest]) {
-          //find neighboring node
-          let nextNode = this.adjacencyList[smallest][neighbor];
-          //calculate new distance to neighboring node
-          let candidate = distances[smallest] + nextNode.weight;
-          let nextNeighbor = nextNode.node;
+          // Find neighboring node.
+          const nextNode = this.adjacencyList[smallest][neighbor];
+
+          // Calculate new distance to neighboring node.
+          const candidate = distances[smallest] + nextNode.weight,
+            nextNeighbor = nextNode.node;
+
           if (candidate < distances[nextNeighbor]) {
-            //updating new smallest distance to neighbor
+            // Updating new smallest distance to neighbor.
             distances[nextNeighbor] = candidate;
-            //updating previous - How we got to neighbor
+
+            // Updating previous - How we got to neighbor.
             previous[nextNeighbor] = smallest;
-            //enqueue in priority queue with new priority
+
+            // Enqueue in priority queue with new priority.
             nodes.enqueue(nextNeighbor, candidate);
           }
         }
       }
     }
+
     return path.concat(smallest).reverse();
   }
 }
-
-var graph = new WeightedGraph()
-graph.addVertex("A");
-graph.addVertex("B");
-graph.addVertex("C");
-graph.addVertex("D");
-graph.addVertex("E");
-graph.addVertex("F");
-
-graph.addEdge("A", "B", 4);
-graph.addEdge("A", "C", 2);
-graph.addEdge("B", "E", 3);
-graph.addEdge("C", "D", 2);
-graph.addEdge("C", "F", 4);
-graph.addEdge("D", "E", 3);
-graph.addEdge("D", "F", 1);
-graph.addEdge("E", "F", 1);
-
-
-graph.Dijkstra("A", "E");
-
-// ["A", "C", "D", "F", "E"]
