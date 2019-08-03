dcb2f0fb244ef350a6c02891c26db42c092ef063
Aviv Haruzi <avivharuzi@gmail.com>
2019-08-03 09:18:10 +0300

    Update merge sort

--- sorting/merge-sort/README.md
+++ sorting/merge-sort/README.md
@@ -1,5 +1,7 @@
 # Merge Sort
 
-* It's a combination of two things - merging and sorting!
+* It's a combination of two things - merging and sorting
 * Exploits the fact that arrays of 0 or 1 element are always sorted
 * Works by decomposing an array into smaller arrays of 0 or 1 elements, then building up a newly sorted array
+
+![Merge Sort](/images/merge-sort.png)
--- sorting/merge-sort/merge-sort.js
+++ sorting/merge-sort/merge-sort.js
@@ -1,50 +1,54 @@
 /**
- * @param {array} arr1
- * @param {array} arr2
- * @return {array}
+ * Merge between two arrays together.
+ *
+ * @param {Array<number>} arr1 - The arr1 value.
+ * @param {Array<number>} arr2 - The arr2 value.
+ * @return {Array<number>}
  */
 function merge(arr1, arr2) {
-  let results = [];
+  const mergedArr = [];
 
-  let i = 0;
-  let j = 0;
+  let i = 0,
+    j = 0;
 
   while (i < arr1.length && j < arr2.length) {
     if (arr2[j] > arr1[i]) {
-      results.push(arr1[i]);
+      mergedArr.push(arr1[i]);
       i++;
     } else {
-      results.push(arr2[j])
+      mergedArr.push(arr2[j])
       j++;
     }
   }
 
   while (i < arr1.length) {
-    results.push(arr1[i])
+    mergedArr.push(arr1[i])
     i++;
   }
 
   while (j < arr2.length) {
-    results.push(arr2[j])
+    mergedArr.push(arr2[j])
     j++;
   }
 
-  return results;
+  return mergedArr;
 }
 
 /**
- * @param {array} arr
- * @return {array}
+ * Sorting the array using merge sort algorithm.
+ *
+ * @param {Array<number>} arr - The arr value.
+ * @return {Array<number>}
  */
 function mergeSort(arr) {
   if (arr.length <= 1) {
     return arr;
   }
 
-  let middle = Math.floor(arr.length / 2);
+  const middle = Math.floor(arr.length / 2);
 
-  let left = mergeSort(arr.slice(0, middle));
-  let right = mergeSort(arr.slice(middle));
+  const left = mergeSort(arr.slice(0, middle));
+  const right = mergeSort(arr.slice(middle));
 
   return merge(left, right);
 }
